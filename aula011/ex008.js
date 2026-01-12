7bcd02f68dc671aed2dbfeae90bb18a88d6990a5
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-01-12 17:30:00 -0300

    exercícios

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //Condição Simples
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança.')
