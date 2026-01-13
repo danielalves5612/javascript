f1881aa3be08459ad740b30b70a54720c5687396
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-01-12 21:34:16 -0300

    fazendo exercícios

---
+++ aula012ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = window.document.getElementById ('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12){
+        img.src = 'manhã.jpg'
+        document.body.style.backgroundColor = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18){
+        img.src = 'tarde.jpg'
+        document.body.style.backgroundColor = '#b9846f'
+    }else {
+        img.src = 'noite.jpg'
+        document.body.style.backgroundColor = '#515154'
+    }
+}
+
+
+
