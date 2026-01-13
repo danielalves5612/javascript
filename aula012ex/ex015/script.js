f8bcbf09bb46caa334b53bfbce4c43ebe9437de9
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-01-13 15:23:31 -0300

    término do verificador de idade

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -1,19 +1,49 @@
 function verificar(){
-    var data = new Date()
+    var data = new Date ()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.lenght == 0 || Number(fano.value) > ano){
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-    }else {
-        var fsex = document.getElementsByTagName('radsex')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERROR] Verifique os dados digitados!')
+    }else{
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var genero = ''
-        if (fsex[0].checked) {
-            genero = 'Homem'
+        var gênero = ''
+        var img = document.createElement ('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            var gênero = 'Homem'
+            if (idade >= 0 && idade <= 10){
+                //Criança
+                img.setAttribute('src', 'homem-criança.jpg')
+            }else if (idade <= 21){
+                //Jovem
+                img.setAttribute('src', 'homem-jovem.jpg')
+            }else if (idade <= 50){
+                //Adulto
+                img.setAttribute('src', 'homem-adulto.jpg')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'homem-idoso.jpg')
+            }
         }else if (fsex[1].checked){
-            genero = 'Mulher'
+            var gênero = 'Mulher'
+            if (idade >= 0 && idade <= 10){
+                //Criança
+                img.setAttribute('src', 'mulher-criança.jpg')
+            }else if (idade <= 21){
+                //Jovem
+                img.setAttribute('src', 'mulher-jovem.jpg')
+            }else if (idade <= 50){
+                //Adulto
+                img.setAttribute('src', 'mulher-adulta.jpg')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'mulher-idosa.jpg')
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.style.textAlign = 'center'
+        res.innerHTML = `Verificamos ${gênero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
