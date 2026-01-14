45e30b7630e6bac6553fb9a3ec0e86dd40c13b47
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-01-14 19:49:56 -0300

    Entendendo vetores

---
+++ aula015/vetornatela.js
@@ -0,0 +1,14 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+//console.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+*/
+for(c = valores; c<valores.length; c++){
+    console.log(`Valores do vetor ${c}`)
+}
