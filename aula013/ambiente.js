47819dc0a4feb1961cacce9e0525348019bc219b
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-01-13 21:54:19 -0300

    Exercício de contador

---
+++ aula013/ambiente.js
@@ -0,0 +1,17 @@
+/*
+var c = 1
+do {
+    console.log('Olá, tudo bem?') //do while
+    c++
+}while(c <= 6)
+*/
+
+/*
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`) //while
+    c++
+}
+*/
+
+//console.log('Tudo bem?')
