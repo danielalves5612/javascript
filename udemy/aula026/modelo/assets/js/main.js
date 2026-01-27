1f0256eb7910cff87e74671f6f95f233b54c4172
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-01-27 20:22:50 -0300

    Terminando o exercício de calculo IMC

--- udemy/aula026/modelo/assets/js/main.js
+++ udemy/aula026/modelo/assets/js/main.js
@@ -8,12 +8,15 @@ function tabelaImc (){
     let altura = document.querySelector('#altura')
     peso = Number(peso.value)
     altura = Number(altura.value)
+    if (peso == 0){
+      resultado.innerHTML = '<p>Por favor, digite um peso válido</p>'
+      resultado.style.backgroundColor = 'red'
+    }else if(altura == 0){
+        resultado.innerHTML = '<p>Por favor, digite uma altura válida</p>'
+        resultado.style.backgroundColor = 'red'
+    }else{
     let imc = peso / (altura * altura)
-
-      if (peso == 0 || altura == 0){
-        alert('Por favor, digite uma altura ou peso')
-    }
-    resultado.innerHTML = ''   
+    resultado.innerHTML = ''
     if(imc >= 0 && imc <= 18.5){
         resultado.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)} (Abaixo do peso)</p>`
     }else if(imc >= 18.5 && imc <= 24.9){
@@ -28,7 +31,73 @@ function tabelaImc (){
         resultado.innerHTML = `<p>Seu IMC é ${imc.toFixed(2)}, (Obesidade grau 3)</p>`
     }
     resultado.style.backgroundColor = 'cyan'
+    }
   }
   form.addEventListener('submit', calcularPeso)
 }
 tabelaImc()
+
+/*
+const form = document.querySelector('#formulario')
+
+    form.addEventListener('submit', function (e)){
+        e.preventDefault()
+        const inputPeso = e.target.querySelector('#peso')
+        const inputAltura = e.target.querySelector('#altura')
+
+        const peso = Number(inputPeso.value)
+        const altura = Number(inputAltura.value)
+
+        if (!peso){
+        setResultado('Peso inválido', false)}
+        return
+        }
+
+        if(!altura){
+        setResultado('Altura inválida', false)
+        return
+        }
+
+        const imc = getImc(peso, altura)
+        const nivelImc = getNivelImc(imc)
+        const msg = `Seu IMC é ${imc} (${getNivelImc})`
+        setResultado(msg, true);
+    }
+
+    function getNivelImc (imc){
+        const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3']
+
+        if(imc >= 39.9) return nivel[5]
+        if(imc >= 34.9) return nivel[4]
+        if(imc >= 29.9) return nivel[3]
+        if(imc >= 24.9) return nivel[2]
+        if(imc >= 18.5) return nivel[1]
+        if(imc < 18.5)  return nivel[0]
+    }
+
+    function getImc(peso, altura){
+        const imc = peso / altura ** 2}
+        return imc.toFixed(2)
+    }
+
+    function criaP(){
+        const p = document.createElement('p')
+        return p
+    }
+
+    function setResultado (msg, isValid){
+        const resultado = document.querySelector('#resultado')
+        resultado.innerHTML = ''
+        const p = criaP()
+
+        if (isValid) { 
+            p.classList.add('paragrafo-resultado')
+        }else{
+            p.classList.add('bad')
+        }
+
+        p.innerHTML = msg
+        resultado.appendChild(p)
+    }
+
+*/
