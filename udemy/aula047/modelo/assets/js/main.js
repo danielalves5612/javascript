63cb9fa49b5462772e51331ce119c71eda52ba45
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-02-05 20:57:53 -0300

    termino do exercício de timer

--- udemy/aula047/modelo/assets/js/main.js
+++ udemy/aula047/modelo/assets/js/main.js
@@ -1,3 +1,30 @@
+function relogio(){
+const relogio = document.querySelector('div#relogio')
+let segundos = 0
+let timer
+
+document.addEventListener('click',function(e){
+    const el = e.target
+
+    if(el.classList.contains('iniciar')){
+        clearInterval(timer)
+        iniciaRelogio ()
+        relogio.classList.remove('pausado')
+    }
+
+    if(el.classList.contains('pausar')){
+        clearInterval(timer)
+        relogio.classList.add('pausado')
+    }
+
+    if(el.classList.contains('zerar')){
+        clearInterval(timer)
+        segundos = 0
+        relogio.innerHTML = '00:00:00'
+        relogio.classList.remove('pausado')
+    }
+})
+
 function criaHoraDosSegundos(segundos){
     const data = new Date(segundos * 1000)
     return data.toLocaleTimeString('pt-BR', {
@@ -6,31 +33,15 @@ function criaHoraDosSegundos(segundos){
     })
 }
 
-const relogio = document.querySelector('div#relogio')
-const iniciar = document.querySelector('button#iniciar')
-const pausar = document.querySelector('button#pausar')
-const zerar = document.querySelector('button#zerar')
-let segundos = 0
-let timer
-
-function iniciaRelogio(){
-    timer = setInterval(function (){
+function iniciaRelogio (){
+     timer = setInterval(function(){
         segundos++
         relogio.innerHTML = criaHoraDosSegundos(segundos)
-    }, 1000)
+    },1000)
 }
+}
+relogio()
 
-iniciar.addEventListener('click', function(evento){
-    clearInterval(timer)
-    iniciaRelogio()
-});
 
-pausar.addEventListener('click', function(evento){
-    clearInterval(timer)
-});
 
-zerar.addEventListener('click', function(evento){
-    clearInterval(timer)
-    segundos = 0
-    relogio.innerHTML = '00:00:00'
-});
+
