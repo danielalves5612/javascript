42ded678fccaa6acd468e2540e8761a79ca537bb
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-02-07 19:14:15 -0300

    Exercício terminado e entendido

--- udemy/aula048/modelo/assets/js/main.js
+++ udemy/aula048/modelo/assets/js/main.js
@@ -2,72 +2,77 @@ const inputTarefa = document.querySelector('.input-tarefa')
 const btnTarefa = document.querySelector('.btn-tarefa')
 const tarefas = document.querySelector('.tarefas')
 
+function criaTarefa(textoInput){ 
+    const li = criaLi()
+    li.innerHTML = textoInput
+    tarefas.appendChild(li)
+    limpaInput()
+    criaBotaoApagar(li)
+    salvarTarefas()
+    
+}
+
 function criaLi(){
     const li = document.createElement('li')
     return li
 }
 
-btnTarefa.addEventListener('click', function(){
-    criarTarefa()
+function limpaInput(){
+    inputTarefa.value = ''
+    inputTarefa.focus()
+}
+
+function criaBotaoApagar(li){
+    li.innerText += ' '
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.setAttribute('title', 'Apagar tarefa')
+    li.appendChild(botaoApagar)
+}
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaDeTarefas = []
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('Apagar', ' ').trim()
+        listaDeTarefas.push(tarefaTexto)
+    }
+
+    const tarefaJSON = JSON.stringify(listaDeTarefas)
+    localStorage.setItem('tarefas', tarefaJSON)
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas')
+    const listaDeTarefas = JSON.parse(tarefas)
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa)
+    }
+}
+
+adicionaTarefasSalvas()
+
+btnTarefa.addEventListener('click', function(){ 
+    if(!inputTarefa.value) return 
+    criaTarefa(inputTarefa.value)
 })
 
 inputTarefa.addEventListener('keypress', function(e){
-   if(e.keyCode === 13){
-    criarTarefa()
-   }
+    const el = e.target
+    if(e.keyCode === 13){
+       if(!inputTarefa.value) return
+       criaTarefa(inputTarefa.value)
+    }
 })
-
-function criarTarefa(){
-    const li = criaLi()
-    if(!inputTarefa.value) return
-    li.innerHTML = inputTarefa.value
-    tarefas.appendChild(li)
-    inputTarefa.value = ''
-    inputTarefa.focus()
-    criarBotaoApagar(li)
-    selecionaTexto()
-    
-}
-
-function criarBotaoApagar(li){
-    const botaoApagar = document.createElement('button')
-    li.appendChild(botaoApagar)
-    botaoApagar.setAttribute('class', 'apagar')
-    botaoApagar.setAttribute('title', 'Apagar texto')
-    botaoApagar.innerHTML = 'Apagar'
-}
 
 document.addEventListener('click', function(e){
     const el = e.target
     if(el.classList.contains('apagar')){
         el.parentElement.remove()
+        salvarTarefas()
     }
 })
-
-function selecionaTexto(){
-    const pegali = document.querySelectorAll('li')
-    const array = []
-    for(let tarefas of pegali){
-        let texto = tarefas.innerText
-        texto = texto.replace('Apagar', ' ').trim()
-        array.push(texto)
-    }
-    const json = JSON.stringify(array)
-    localStorage.setItem('tarefas', json)
-    carregarTarefas(array)
-}
-
-function carregarTarefas(){
-    let guardaValor = localStorage.getItem('tarefas')
-    if(guardaValor){
-        guardaValor = JSON.parse(guardaValor)
-        for(valor of guardaValor){
-            const li = criaLi()
-            li.innerHTML = valor
-            tarefas.appendChild(li)
-            criarBotaoApagar(li)
-        }
-    }
-}
 
 
